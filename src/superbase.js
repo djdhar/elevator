dc9d9858f23a2526e216e43039cade5c5ffac0fb
Dibyajyoti Dhar 🤖 <dibyajyotidhar@gmail.com>
2024-03-17 22:38:24 +0530

    added sb key

--- src/superbase.js
+++ src/superbase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://xgmhvmztudrhaocnqfwn.supabase.co'
-const supabaseKey = ''
+const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase
